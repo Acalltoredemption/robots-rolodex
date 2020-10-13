8c74f0ab075a4f516f41a78a492b79623f3bcf26
Acalltoredemption <acalltoredemption@gmail.com>
2020-10-13 12:13:28 -0700

    Updates

---
+++ precache-manifest.fbb37d562d9ad31f80ef4bc7e1835ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcab40f900fec16f93b9acf7e94ad7c2",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "6ba3d8b5c00f381eacc7",
+    "url": "/robots-rolodex/static/css/main.82253ac7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/robots-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba3d8b5c00f381eacc7",
+    "url": "/robots-rolodex/static/js/main.177f6034.chunk.js"
+  },
+  {
+    "revision": "02542f5a8b44e87af7b4",
+    "url": "/robots-rolodex/static/js/runtime-main.07f6b5a4.js"
+  }
+]);
